a2aa94f766d5a636d520df925b91507b67ef153e
Robert <robjleal00@gmail.com>
2023-09-21 14:49:11 +0200

    video stream support added

---
+++ stream.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+from deepface import DeepFace 
+import matplotlib.pyplot as plt 
+import cv2
+import pandas as pd
+from glob import glob
+import os
+
+
+
+DeepFace.stream(db_path="C:/Users/robjl/Documents/Python Playground/Spotify Emotion Automator/spotifyEmotionAutomator/spotifyEmotionBasedRecommenderSystem/images/robert", source=0)
+
+
